b62edf2b7197b695faf0fbfd699831e3ce042e6d
DIKE JOHNSON <Johnsondikee@gmail.com>
2022-11-02 18:04:52 +0100

    Api

---
+++ musicapp/views.py
@@ -0,0 +1,24 @@
+from rest_framework import generics
+
+from .models import Song, Artiste
+from .serializers import SongSerializer, ArtisteSerializer
+
+
+class SongList(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
+class ArtisteList(generics.ListCreateAPIView):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializer
+
+
+class ArtisteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializer
